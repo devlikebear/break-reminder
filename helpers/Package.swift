973cdeb81181fc96769e9948baa56353a6cff1b6
devlikebear <devlikebear@gmail.com>
2026-04-17 21:11:16 +0900

    chore: bump macOS deployment target to 14 for SwiftUI onKeyPress

--- helpers/Package.swift
+++ helpers/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "BreakReminderHelpers",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     targets: [
         .target(
             name: "HelperCore"
